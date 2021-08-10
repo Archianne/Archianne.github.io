b9b117f72c4ff9cde36fcd51f71ff1a62d50645b
Archianne <helena19w@gmail.com>
2021-08-10 21:26:52 +0100

    Updates

--- static/js/13.58361068.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonparchianne=this.webpackJsonparchianne||[]).push([[13],{79:function(n,e,c){"use strict";c.r(e);var s=c(0),i=c(7);e.default=function(){return Object(i.jsx)(s.Suspense,{fallback:Object(i.jsx)("div",{children:"Loading"}),children:Object(i.jsx)("p",{children:"In progress"})})}}}]);
-//# sourceMappingURL=13.58361068.chunk.js.map
