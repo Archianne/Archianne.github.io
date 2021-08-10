ba07af1af3c084c13128f6c95c40ff3c0f7c159d
Archianne <helena19w@gmail.com>
2021-08-10 21:24:30 +0100

    animation main screen

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,71 @@ import { Suspense } from "react";
 import styled from "styled-components";
 
 const Home = () => {
+  const TxtRotate = function (element, toRotate, period) {
+    this.toRotate = toRotate;
+    this.element = element;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.element.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function () {
+    var elements = document.getElementsByClassName("txt-rotate");
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute("data-rotate");
+      var period = elements[i].getAttribute("data-period");
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  };
+
   return (
     <StyledHome>
       <Suspense fallback={<div>Loading</div>}>
-        <h1>Hello World</h1>
+        <h1>
+          Hello{" "}
+          <span
+            class="txt-rotate"
+            data-period="2000"
+            data-rotate='[ "World", "JS", "React", "You!" ]'
+          ></span>
+        </h1>
       </Suspense>
     </StyledHome>
   );
